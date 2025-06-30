463fdf2a9a4304f2dd89a4436be3cb836f12e851
Alexsanjr <alex.sandro.1199@gmail.com>
2025-06-30 10:58:09 -0300

    created veiculoDTO

---
+++ backend/src/main/java/com/cefet/vocealuga/dtos/VeiculoDTO.java
@@ -0,0 +1,100 @@
+package com.cefet.vocealuga.dtos;
+
+import com.cefet.vocealuga.entities.enums.Cor;
+import com.cefet.vocealuga.entities.enums.Grupo;
+import com.cefet.vocealuga.entities.enums.StatusVeiculo;
+
+public class VeiculoDTO {
+    private Long id;
+    //Estoque
+    private String marca;
+    private String modelo;
+    private Grupo grupo;
+    private int ano;
+    private Cor cor;
+    private double valorDiaria;
+    private Long quilometragem;
+    private StatusVeiculo statusVeiculo;
+    private String placa;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Cor getCor() {
+        return cor;
+    }
+
+    public void setCor(Cor cor) {
+        this.cor = cor;
+    }
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    public void setValorDiaria(double valorDiaria) {
+        this.valorDiaria = valorDiaria;
+    }
+
+    public Long getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(Long quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public StatusVeiculo getStatusVeiculo() {
+        return statusVeiculo;
+    }
+
+    public void setStatusVeiculo(StatusVeiculo statusVeiculo) {
+        this.statusVeiculo = statusVeiculo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
