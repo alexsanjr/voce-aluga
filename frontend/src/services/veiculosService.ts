03bac4f807df4db6d9f666e91a2456bc84e854c5
levyath <levyarthur2@gmail.com>
2025-07-12 20:18:29 -0300

    para buscar o getall sem authorization

--- frontend/src/services/veiculosService.ts
+++ frontend/src/services/veiculosService.ts
@@ -4,9 +4,10 @@ import api from "./api";
 const API_URL = "/veiculos";
 
 export const getVeiculos = async (marca = "") => {
-  const response = await api.get(API_URL, {
-    params: { marca },
-  });
+  const response = await api.get(
+    API_URL, 
+    { params: { marca }, headers: { Authorization: "" }},
+  );
   console.log("Veículos recebidos:", response.data);
   return response.data;
 };
